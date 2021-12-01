edc660005c68ceaa59b76d14f15c309e73ccf396
Faruq <faruqabdulsalam@yahoo.com>
2021-12-01 21:58:18 +0100

    Change falsy in sample assignment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import shell
 
 while True:
     try:
-        sample="""if (var_1 == 2, 0, if (var_2 == 4, 15, 0) ) + if (var_2 == 3, 5, 0) - if (var_4 == 2, 0, 5) + if (var_3 == 3, 5, 0) """
+        sample="""if (var_1 == 2, if (var_2 == 4, 15, 0), 0 ) + if (var_2 == 3, 5, 0) - if (var_4 == 2, 0, 5) + if (var_3 == 3, 5, 0) """
         inpt = input("Enter variable as object:  ")
         shell.run(sample, inpt)
     except Exception as e:
